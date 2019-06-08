8caba962bd0ece9314baa146a5fa28c3bd8e6cdc
David Ressman <davidr@ressman.org>
2019-06-08 22:21:34 +0100

    add stupid timer flag

--- project-euler/euler.py
+++ project-euler/euler.py
@@ -19,7 +19,13 @@ def main():
     if len(sys.argv) == 0:
         usage()
 
-    ex_no = sys.argv.pop(0)
+    first_arg = sys.argv.pop(0)
+    exercise_timer = True if first_arg == '-t' else False
+
+    if exercise_timer:
+        ex_no = sys.argv.pop(0)
+    else:
+        ex_no = first_arg
 
     if not ex_no.isdigit():
         usage(f"{ex_no} must be numeric")
@@ -35,12 +41,13 @@ def main():
     result = euler_problem.compute(*int_args)
     elapsed_time = timer() - start
 
-    if elapsed_time < 2:
-        num_calls = int(2 // elapsed_time)
-        start = timer()
-        for x in range(0, num_calls):
-            euler_problem.compute(*int_args)
-        elapsed_time = (timer() - start) / num_calls
+    if exercise_timer:
+        if elapsed_time < 2:
+            num_calls = int(2 // elapsed_time)
+            start = timer()
+            for x in range(0, num_calls):
+                euler_problem.compute(*int_args)
+            elapsed_time = (timer() - start) / num_calls
 
     print(f"{result}\t\t(in {elapsed_time:0.3f}s)")
 
